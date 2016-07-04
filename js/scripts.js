ccc89ed6e62016fd9eb7d5f1df22bfacf5d2817c
Edwin <me@edwinmah.com>
2016-07-04 16:38:30 -0400

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // set media query condition
+  var mediaQuery = '(min-width: 56.25em)';
+
+  if (matchMedia(mediaQuery).matches) {
+    
+    // instantiate backstretch.js if media query condition matches
+    $("body").backstretch("images/girl.jpg");
+    
+  } else {
+    
+    // set path to small profile image
+    var imgUrl = 'images/girl-small.jpg';
+    
+    // create markup for small profile image
+    var hero = '<div class="hero"><img src="' + imgUrl + '" alt="my profile image"></div>';
+    
+    // inject small profile image immediately after .skip-content nav in markup
+    $(".skip-content").after(hero);
+  }
+});
+
+// reload page on resize
+window.addEventListener('resize', function () {
+  "use strict";
+  window.location.reload();
+});
