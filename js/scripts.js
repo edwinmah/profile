752cf1ba270834573f3d07d3017dc2c04f3217bf
Edwin <me@edwinmah.com>
2016-07-22 07:25:14 -0400

    removed resize function

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,19 @@ $(document).ready(function() {
   var mediaQuery = '(min-width: 56.25em)';
 
   if (matchMedia(mediaQuery).matches) {
-    
+
     // instantiate backstretch.js if media query condition matches
     $("body").backstretch("images/girl.jpg");
-    
+
   } else {
-    
+
     // set path to small profile image
     var imgUrl = 'images/girl-small.jpg';
-    
+
     // create markup for small profile image
     var hero = '<div class="hero"><img src="' + imgUrl + '" alt="my profile image"></div>';
-    
+
     // inject small profile image immediately after .skip-content nav in markup
     $(".skip-content").after(hero);
   }
-});
-
-// reload page on resize
-window.addEventListener('resize', function () {
-  "use strict";
-  window.location.reload();
 });
